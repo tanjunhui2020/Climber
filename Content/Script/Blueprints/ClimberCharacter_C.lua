31c0dfe8a62abb0801a921bd659741118491b7d9
TanJunhui <tanjunhui2020@outlook.com>
2020-06-14 18:24:32 +0800

    攀爬到顶部时跳上去

--- Content/Script/Blueprints/ClimberCharacter_C.lua
+++ Content/Script/Blueprints/ClimberCharacter_C.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f94fe881d5d2b496f8b6d6c524bedf8c08fb66241ef4930b96fe4c30176148d0
-size 7194
+oid sha256:d91cc00f70cf7bbfd73fe143766509508cb8c89b97d9f8975f65c6799ed2c07e
+size 9735
--- Content/Script/Mannequin/Animations/ClimberAnimInstance_C.lua
+++ Content/Script/Mannequin/Animations/ClimberAnimInstance_C.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c49c498ad61542e79380dbed366a037eb51a1728cc1e31478898fc40c6b4757f
-size 2097
+oid sha256:2a3bf1d5c24282d097d039fa922a113bc392f578527cddebad5681157adde663
+size 2433
